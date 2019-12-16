80551e3c199d9a9df438feb7da70a82b84053dc3
João Mota <47133204+jppm99@users.noreply.github.com>
2019-12-16 14:39:40 +0000

    removed rtt stuff

--- XMLscene.js
+++ XMLscene.js
@@ -136,8 +136,7 @@ class XMLscene extends CGFscene {
         this.selectedView = this.graph.defaultViewID;
         this.rttView = this.graph.defaultViewID;
 
-        this.securityCamera = new MySecurityCamera(this);
-        this.updateCamera(false, true);
+        this.updateCamera(true);
 
         this.scShader = new CGFshader(this.gl, "shaders/sc.vert", "shaders/sc.frag");
 
@@ -152,19 +151,17 @@ class XMLscene extends CGFscene {
         // Kinda TODO
     }
 
-    async updateCamera(rtt, update, smooth){
+    async updateCamera(update, smooth){
         //console.log("*********** Changed View to " + this.selectedView + " ***********");
 
         // uncomment to log current view parameters
         //for(var key in this.graph.views[this.selectedView]) if(this.graph.views[this.selectedView].hasOwnProperty(key)) console.log("KEY: " + key + " -- VALUE: " + this.graph.views[this.selectedView][key]);
 
         if(update) {
-            let view = this.graph.views[(rtt ? this.rttView : this.selectedView)];
+            let view = this.graph.views[this.selectedView];
 
             if (view[0] == 'perspective')
-                if(rtt)
-                    this.RTTcam = new CGFcamera(view[3] * DEGREE_TO_RAD, view[1], view[2], view[4], view[5]);
-                else if (smooth == true) {
+                if (smooth == true) {
                     let animDuration = 2; // camera transition duration in seconds
                     let frames = animDuration / (this.SCENE_UPDATE_PERIOD/1000);
 
@@ -197,21 +194,20 @@ class XMLscene extends CGFscene {
                 }
         }
 
-        if(!rtt) this.gui.setActiveCamera(this.NormalCam);
+        this.gui.setActiveCamera(this.NormalCam);
     }
 
     /**
      * Displays the scene.
      */
-    render(rtt) {
+    render() {
         // ---- BEGIN Background, camera and axis setup
 
         // Clear image and depth buffer everytime we update the scene
         this.gl.viewport(0, 0, this.gl.canvas.width, this.gl.canvas.height);
         this.gl.clear(this.gl.COLOR_BUFFER_BIT | this.gl.DEPTH_BUFFER_BIT);
 
-        if(rtt) this.camera = this.RTTcam;
-        else this.camera = this.NormalCam;
+        this.camera = this.NormalCam;
         this.gui.setActiveCamera(this.camera);
 
 
@@ -265,15 +261,7 @@ class XMLscene extends CGFscene {
 
         let update;
 
-        // RTT  -> last rtt comparison added for performance reasons
-        if(this.lastRttView === this.rttView) update = false;
-        else {
-            this.lastRttView = this.rttView;
-            update = true;
-        }
-        this.updateCamera(true, true);
-        this.rtt.attachToFrameBuffer();
-        this.render(true);
+       
 
         // NORMAL
         if(this.lastSelectedView === this.selectedView) update = false;
@@ -281,12 +269,11 @@ class XMLscene extends CGFscene {
             this.lastSelectedView = this.selectedView;
             update = true;
         }
-        this.updateCamera(false, update, true);
-        this.rtt.detachFromFrameBuffer();
-        this.render(false);
+        this.updateCamera(true, true);
+        this.render();
 
         // -----
-        this.rtt.bind();
+        /*this.rtt.bind();
 
         this.gl.disable(this.gl.DEPTH_TEST);
         this.setActiveShader(this.scShader);
@@ -294,7 +281,7 @@ class XMLscene extends CGFscene {
         this.scShader.setUniformsValues({time : this.y % 1, height : this.gl.canvas.height, width : this.gl.canvas.width});
         this.securityCamera.display();
         this.setActiveShader(this.defaultShader);
-        this.gl.enable(this.gl.DEPTH_TEST);
+        this.gl.enable(this.gl.DEPTH_TEST);*/
     }
 
     update(currTime) {
